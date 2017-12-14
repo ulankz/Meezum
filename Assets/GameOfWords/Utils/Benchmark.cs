b5bd72c79ef4e0da4b127c0763c822273ad3a24f
Ulan Assanov <Ulan@users-iMac-2.local>
2017-12-14 16:15:11 +0600

    adds scripts for checking runtime of code for benchmarking
    Former-commit-id: 33be0924106248c5c5ec343b4e3b2f7a987e8592

---
+++ Assets/GameOfWords/Utils/Benchmark.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+public class Benchmark : IDisposable 
+{
+	private readonly Stopwatch timer = new Stopwatch();
+	private readonly string benchmarkName;
+
+	public Benchmark(string benchmarkName)
+	{
+		this.benchmarkName = benchmarkName;
+		timer.Start();
+	}
+
+	public void Dispose() 
+	{
+		timer.Stop();
+		Debug.Log(benchmarkName + " " + timer.Elapsed);
+	}
+}
