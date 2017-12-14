081000e792ec0458b1b553c78e8da5cef003ffed
Ulan Assanov <Ulan@users-iMac-2.local>
2017-12-14 16:05:11 +0600

    adds scripts for showing properties in inspector panel
    Former-commit-id: a51049fdae99f1774095ab6c0d7ff1a32d4fc980

---
+++ Assets/Editor/MyTypeEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor( typeof( GameWordsManager ) )]
+public class MyTypeEditor : Editor {
+
+
+	GameWordsManager m_Instance;
+	PropertyField[] m_fields;
+
+
+	public void OnEnable()
+	{
+		m_Instance = target as GameWordsManager;
+		m_fields = ExposeProperties.GetProperties( m_Instance );
+	}
+
+	public override void OnInspectorGUI () {
+
+		if ( m_Instance == null )
+			return;
+
+		this.DrawDefaultInspector();
+
+		ExposeProperties.Expose( m_fields );
+
+	}
+}
