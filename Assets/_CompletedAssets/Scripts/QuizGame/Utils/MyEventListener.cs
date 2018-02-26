cb04b96690c9860954e482f58cef0b30a16a69b7
Ulan Assanov <Ulan@users-iMac-2.local>
2018-02-26 13:14:27 +0600

    adds script MyEventListener for double click check
    Former-commit-id: 91d7dd5dd92fbc43a26e3d92ba68b716baa86533

---
+++ Assets/_CompletedAssets/Scripts/QuizGame/Utils/MyEventListener.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MyEventListener : MonoBehaviour, IPointerClickHandler
+{
+
+	private float clickTime;            // time of click
+	public bool onClick = true;            // is click allowed on button?
+	public bool onDoubleClick = false;    // is double-click allowed on button?
+
+	
+	public void OnPointerClick(PointerEventData data)
+	{      
+		int clickCount = 1; // single click
+		
+		// get interval between this click and the previous one (check for double click)
+		float interval = data.clickTime - clickTime;
+		
+		// if this is double click, change click count
+		if (interval < 1.5 && interval > 0)
+			clickCount = 2;
+		
+		// reset click time
+		clickTime = data.clickTime;
+		
+		// single click
+		if (onClick && clickCount == 1)
+		{
+
+			// enter code here  
+		}
+		
+		// double click
+		if (onDoubleClick && clickCount == 2)
+		{
+			// enter code here
+		}
+		
+	}
+}
