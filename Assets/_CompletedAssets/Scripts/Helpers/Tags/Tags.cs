5bdd14cd911ac5b9c2b7095c1ca487688b03cfaa
Ulan Assanov <Ulan@users-iMac-2.local>
2018-02-26 10:57:15 +0600

    adds global Tags script
    Former-commit-id: 370dc1734d6ea4ee39a119cc0133f5a519b031a3

---
+++ Assets/_CompletedAssets/Scripts/Helpers/Tags/Tags.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MeezumGame
+{
+	public class Tags
+	{
+		public const string STAR_MANAGER = "StarManager";
+		public const string TILE = "Tile";
+		public const string CELL = "Cell";
+		public const string LETTERS = "Letters";
+		public const string CELLS = "Cells";
+		public const string LETTER_PREFAB = "LetterPrefab";
+		public const string PLAYER = "Player";
+		public const string QUESTION = "Question";
+	}
+}
