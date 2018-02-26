e15be8bf62245242f2fa7966d23f529b84d5ab2e
Ulan Assanov <Ulan@users-iMac-2.local>
2018-02-26 17:02:27 +0600

    adds custom QuizButton which inherits from UI.Button
    Former-commit-id: 01a42686456490e55424664b7c9f676228e467c8

---
+++ Assets/Editor/QButtonEditor.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+using QuizGame;
+[CustomEditor(typeof(QuizButton))]
+	public class QButtonEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+			
+		}
+
+}
--- Assets/_CompletedAssets/Scripts/QuizGame/Models/QuizButton.cs
+++ Assets/_CompletedAssets/Scripts/QuizGame/Models/QuizButton.cs
@@ -5,22 +5,50 @@ using UnityEngine.UI;
 
 namespace QuizGame
 {
+	[System.Serializable]
 	public class QuizButton : Button
 	{
+
 		#region PRIVATE MEMBERS
+		protected QuizButton() : base(){
+		
+		}
 		[SerializeField]
 		private Text
 			buttonLablel;
+		[SerializeField]
+		private Image
+			buttonImage;
+		[SerializeField]
+		private Sprite[] stateImages;// 3 sprites in the array || 0-index: default sprite || 1-index: correct sprite || 2- index: wrong sprite
+
+
 		#endregion
 		void Awake ()
 		{
 			buttonLablel = gameObject.transform.GetComponentInChildren<Text> ();
+			buttonImage = gameObject.GetComponent<Image>();
 		}
+
 		#region PUBLIC MEMBERS
 		public void UpdateButton(string description){
 			if (buttonLablel != null)
 				buttonLablel.text = description;
 		}
+		public void SetDefaultState(){
+			buttonImage.sprite = stateImages [0];// Sets  sprite for correct state
+		}
+		public void SetCorrectChoiceState(){
+			buttonImage.sprite = stateImages [1];// Sets  sprite for correct state
+		}
+		public void SetWrongChoiceState(){
+			buttonImage.sprite = stateImages [2];// Sets  sprite for correct state
+		}
+		public void SetInProcessState(){
+			buttonImage.sprite = stateImages [0];// Sets  sprite for correct state
+		}
+
 		#endregion
 	}
+		
 }
