5b9d2454812e428eb92342117e329dd610ad2726
Ulan Assanov <Ulan@users-iMac-2.local>
2018-03-02 11:36:24 +0600

    adds SoundInstruction classes for handling game instruction sounds

---
+++ Assets/_CompletedAssets/Scripts/Controllers/SoundManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using QuizGame;
+namespace MeezumGame
+{
+	public class SoundManager : MonoBehaviour
+	{
+		#region PRIVATE MEMBERS
+		[SerializeField]
+		private Dictionary<string,InstructionSounds> instructionSounds = new Dictionary<string, InstructionSounds>();
+		[SerializeField]
+		private AudioSource audioSource;
+		[SerializeField]
+		private InstructionSounds currentInstructionSoundInstance;
+		#endregion
+		#region SYSTEM METHODS
+		private void Start(){
+			audioSource = gameObject.AddComponent<AudioSource> ();
+			PopulateWholeInstructionSounds ();
+		}
+
+		#endregion
+		#region PUBLIC METHODS
+
+		public void PlayGameRule(string miniGame){
+			if (instructionSounds.TryGetValue (miniGame,out currentInstructionSoundInstance)){
+				audioSource.clip = currentInstructionSoundInstance.GameRule;
+				if(!audioSource.isPlaying)
+				audioSource.Play ();		
+			}
+		}
+		public void PlayCallToAction(string miniGame){
+			if (instructionSounds.TryGetValue (miniGame,out currentInstructionSoundInstance)){
+				audioSource.clip = currentInstructionSoundInstance.CallToAction;
+				if(!audioSource.isPlaying)
+					audioSource.Play ();		
+			}
+		}
+
+		#endregion
+		#region PRIVATE METHODS
+		private void PopulateWholeInstructionSounds(){
+			InstructionSounds quizGameSounds = new InstructionSoundVictorina ();
+			instructionSounds.Add ("QuizGame", quizGameSounds);
+		}
+		#endregion
+
+	}
+}
---
+++ Assets/_CompletedAssets/Scripts/QuizGame/Models/InstructionSoundVictorina.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MeezumGame;
+namespace QuizGame
+{	[System.Serializable]
+	public class InstructionSoundVictorina : InstructionSounds
+	{
+		#region PUBLIC MEMBERS
+		private static int correctSoundsSize = 2;
+		private static int wrongSoundSize = 2;
+		private AudioClip[] corrects = new AudioClip[correctSoundsSize];
+		private AudioClip[] wrongs = new AudioClip[wrongSoundSize];
+		private AudioClip end;
+		#endregion
+		#region CONSTRUCTORS
+		public InstructionSoundVictorina():base(){
+			Debug.Log ("INSTRUCTION_SOUNDS_VICTORINA_DEFAULT CONSTRUCTOR IS CALLED");
+		}
+		#endregion
+		#region PROPERTY MEMBERS
+		public AudioClip[] Corrects {
+			get {
+				return this.corrects;
+			}
+			set {
+				corrects = value;
+			}
+		}
+
+		public AudioClip[] Wrongs {
+			get {
+				return this.wrongs;
+			}
+			set {
+				wrongs = value;
+			}
+		}
+
+		public AudioClip End {
+			get {
+				return this.end;
+			}
+			set {
+				end = value;
+			}
+		}
+		#endregion
+
+		#region PROTECTED METHODS
+		protected override void LoadAudioClips ()
+		{
+			base.LoadAudioClips ();
+			GameRule = Resources.Load ("Sounds/QuizGame/gameRule", typeof(AudioClip)) as AudioClip;
+			CallToAction = Resources.Load ("Sounds/QuizGame/callToAction", typeof(AudioClip)) as AudioClip;
+			Corrects[0] = Resources.Load ("Sounds/QuizGame/correct_0", typeof(AudioClip)) as AudioClip;
+			Corrects[1] = Resources.Load ("Sounds/QuizGame/correct_1", typeof(AudioClip)) as AudioClip;
+//			corrects[2] = Resources.Load ("Sounds/QuizGame/correct_2", typeof(AudioClip)) as AudioClip;
+//			corrects[3] = Resources.Load ("Sounds/QuizGame/correct_3", typeof(AudioClip)) as AudioClip;
+			Wrongs[0] = Resources.Load ("Sounds/QuizGame/wrong_0", typeof(AudioClip)) as AudioClip;
+			Wrongs[1] = Resources.Load ("Sounds/QuizGame/wrong_1", typeof(AudioClip)) as AudioClip;
+//			wrongs[2] = Resources.Load ("Sounds/QuizGame/wrong_2", typeof(AudioClip)) as AudioClip;
+//			wrongs[3] = Resources.Load ("Sounds/QuizGame/wrong_3", typeof(AudioClip)) as AudioClip;
+		}	
+		#endregion
+	}
+}
