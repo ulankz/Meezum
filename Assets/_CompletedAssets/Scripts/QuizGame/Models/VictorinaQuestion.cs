2086d46837a54166f4697f60f8eb5f494a9da411
Ulan Assanov <Ulan@users-iMac-2.local>
2018-03-06 16:29:31 +0600

    updates VictorinaQuestion for handling question sounds

--- Assets/_CompletedAssets/Scripts/QuizGame/Models/VictorinaQuestion.cs
+++ Assets/_CompletedAssets/Scripts/QuizGame/Models/VictorinaQuestion.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 namespace QuizGame{
 [System.Serializable]
 public class VictorinaQuestion {
+	#region PRIVATE MEMBERS
+	private AudioClip questionSound;
 	[SerializeField]private string description;
-	[SerializeField]private string[] answers;
+	[SerializeField]private string[] answers;	
 	[SerializeField]private int correctIndex;
+	#endregion
 
+	#region PUBLIC PROPERTIES
+	AudioClip QuestionSound {
+		get {
+			return this.questionSound;
+		}
+		set {
+			questionSound = value;
+		}
+	}
 	//[ExposeProperty]
 	public string Description {
 		get {
@@ -35,10 +47,22 @@ public class VictorinaQuestion {
 			correctIndex = value;
 		}
 	}
-	public VictorinaQuestion(string desc,string[] ans, int correctIndex){
+	#endregion
+	#region  CONSTRUCTOR METHODS
+	public VictorinaQuestion(string desc,string[] ans, int correctIndex,string audioPath){
 		description = desc;
 		answers = ans;
 		this.correctIndex = correctIndex;
+		LoadAudios (audioPath);
+	}
+	#endregion
+	#region PUBLIC METHODS
+	public void PlayQuestionSound(){
+		
+	}
+	#endregion
+	private void LoadAudios(string path){//"Sounds/QuizGame/callToAction"
+		QuestionSound = Resources.Load (path, typeof(AudioClip)) as AudioClip;
 	}
 }
 }
