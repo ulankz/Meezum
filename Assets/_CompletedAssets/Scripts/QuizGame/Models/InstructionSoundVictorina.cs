5d40e8049f5b022c8868e2cdefbc59a4285d7f98
Ulan Assanov <Ulan@users-iMac-2.local>
2018-03-06 16:32:02 +0600

    updates InstructionSoundVictorina class for handling first click call to action sound instruction

--- Assets/_CompletedAssets/Scripts/QuizGame/Models/InstructionSoundVictorina.cs
+++ Assets/_CompletedAssets/Scripts/QuizGame/Models/InstructionSoundVictorina.cs
@@ -6,16 +6,17 @@ namespace QuizGame
 {	[System.Serializable]
 	public class InstructionSoundVictorina : InstructionSounds
 	{
-		#region PUBLIC MEMBERS
+		#region PRIVATE MEMBERS
 		private static int correctSoundsSize = 2;
 		private static int wrongSoundSize = 2;
 		private AudioClip[] corrects = new AudioClip[correctSoundsSize];
 		private AudioClip[] wrongs = new AudioClip[wrongSoundSize];
 		private AudioClip end;
+		private AudioClip firstClickCallToAction;
 		#endregion
 		#region CONSTRUCTORS
 		public InstructionSoundVictorina():base(){
-			Debug.Log ("INSTRUCTION_SOUNDS_VICTORINA_DEFAULT CONSTRUCTOR IS CALLED");
+			//Debug.Log ("INSTRUCTION_SOUNDS_VICTORINA_DEFAULT CONSTRUCTOR IS CALLED");
 		}
 		#endregion
 		#region PROPERTY MEMBERS
@@ -45,14 +46,25 @@ namespace QuizGame
 				end = value;
 			}
 		}
+		public AudioClip FirstClickCallToAction {
+			get {
+				return this.firstClickCallToAction;
+			}
+			set {
+				firstClickCallToAction = value;
+			}
+		}
 		#endregion
 
 		#region PROTECTED METHODS
 		protected override void LoadAudioClips ()
-		{
+		{	
 			base.LoadAudioClips ();
+
 			GameRule = Resources.Load ("Sounds/QuizGame/gameRule", typeof(AudioClip)) as AudioClip;
 			CallToAction = Resources.Load ("Sounds/QuizGame/callToAction", typeof(AudioClip)) as AudioClip;
+			FirstClickCallToAction = Resources.Load ("Sounds/QuizGame/callToAction", typeof(AudioClip)) as AudioClip;
+			End = Resources.Load ("Sounds/QuizGame/correct_0", typeof(AudioClip)) as AudioClip;
 			Corrects[0] = Resources.Load ("Sounds/QuizGame/correct_0", typeof(AudioClip)) as AudioClip;
 			Corrects[1] = Resources.Load ("Sounds/QuizGame/correct_1", typeof(AudioClip)) as AudioClip;
 //			corrects[2] = Resources.Load ("Sounds/QuizGame/correct_2", typeof(AudioClip)) as AudioClip;
@@ -61,6 +73,7 @@ namespace QuizGame
 			Wrongs[1] = Resources.Load ("Sounds/QuizGame/wrong_1", typeof(AudioClip)) as AudioClip;
 //			wrongs[2] = Resources.Load ("Sounds/QuizGame/wrong_2", typeof(AudioClip)) as AudioClip;
 //			wrongs[3] = Resources.Load ("Sounds/QuizGame/wrong_3", typeof(AudioClip)) as AudioClip;
+			Debug.Log ("LOAD_AUDIO_CLIPS : AudioClips loaded successfully");
 		}	
 		#endregion
 	}
