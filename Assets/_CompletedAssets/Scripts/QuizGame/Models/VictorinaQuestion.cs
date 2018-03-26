bc177de489a8c52498057701e914596f2f913821
Ulan Assanov <Ulan@users-iMac-2.local>
2018-03-26 14:49:03 +0600

    adds logic for playing and loading question and answer sounds

--- Assets/_CompletedAssets/Scripts/QuizGame/Models/VictorinaQuestion.cs
+++ Assets/_CompletedAssets/Scripts/QuizGame/Models/VictorinaQuestion.cs
@@ -5,19 +5,35 @@ namespace QuizGame{
 [System.Serializable]
 public class VictorinaQuestion {
 	#region PRIVATE MEMBERS
+	[SerializeField]
+	private int questionID = 0;
+	[SerializeField]
 	private AudioClip questionSound;
+	[SerializeField]
+	private AudioClip[] answerSounds = new AudioClip[4];
+	[SerializeField]
+	private string soundPath = "Sounds/QuizGame/";
 	[SerializeField]private string description;
 	[SerializeField]private string[] answers;	
 	[SerializeField]private int correctIndex;
 	#endregion
 
 	#region PUBLIC PROPERTIES
-	AudioClip QuestionSound {
+
+	public AudioClip QuestionSound {
 		get {
 			return this.questionSound;
 		}
 		set {
 			questionSound = value;
+		}
+	}
+	public AudioClip[] AnswerSounds {
+		get {
+			return this.answerSounds;
+		}
+		set {
+			answerSounds = value;
 		}
 	}
 	//[ExposeProperty]
@@ -49,20 +65,23 @@ public class VictorinaQuestion {
 	}
 	#endregion
 	#region  CONSTRUCTOR METHODS
-	public VictorinaQuestion(string desc,string[] ans, int correctIndex,string audioPath){
-		description = desc;
-		answers = ans;
+	public VictorinaQuestion(int id,string desc,string[] ans, int correctIndex){
+		this.questionID = id;;
+		this.description = desc;
+		this.answers = ans;
 		this.correctIndex = correctIndex;
-		LoadAudios (audioPath);
+		LoadAudios (soundPath);
 	}
 	#endregion
 	#region PUBLIC METHODS
-	public void PlayQuestionSound(){
-		
-	}
+
 	#endregion
-	private void LoadAudios(string path){//"Sounds/QuizGame/callToAction"
-		QuestionSound = Resources.Load (path, typeof(AudioClip)) as AudioClip;
+	private void LoadAudios(string path){	//"Sounds/QuizGame
+		QuestionSound = Resources.Load (path+"Question_"+questionID+"/question", typeof(AudioClip)) as AudioClip;
+		AnswerSounds[0] = Resources.Load (path+"Question_"+questionID+"/Answers/answer_0", typeof(AudioClip)) as AudioClip; 
+		AnswerSounds[1] = Resources.Load (path+"Question_"+questionID+"/Answers/answer_1", typeof(AudioClip)) as AudioClip; 
+		AnswerSounds[2] = Resources.Load (path+"Question_"+questionID+"/Answers/answer_2", typeof(AudioClip)) as AudioClip;
+		AnswerSounds[3] = Resources.Load (path+"Question_"+questionID+"/Answers/answer_3", typeof(AudioClip)) as AudioClip;
 	}
 }
 }
