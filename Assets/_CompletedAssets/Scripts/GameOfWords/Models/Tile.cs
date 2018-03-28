5ea37ff43d2de8118a42a29343eb3b2172f1023e
Ulan Assanov <Ulan@users-iMac-2.local>
2018-03-28 11:48:44 +0600

    updates scripting for refactoring

--- Assets/_CompletedAssets/Scripts/GameOfWords/Models/Tile.cs
+++ Assets/_CompletedAssets/Scripts/GameOfWords/Models/Tile.cs
@@ -1,97 +1,106 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace GameOfWords{
-public class Tile : MonoBehaviour {
-
-	private Vector2 startingPosition;
-	private List<Transform> touchingTiles;
-	private Transform myParent;
-	private AudioSource audSource;
-
-	// Use this for initialization
-	void Start () {
-		startingPosition = transform.position;
-		touchingTiles = new List<Transform> ();
-		myParent = transform.parent;
-		audSource = gameObject.GetComponent<AudioSource> ();
-	}
-
-	public void PickUp()
+using MeezumGame;
+namespace GameOfWords
+{
+	public class Tile : MonoBehaviour
 	{
-		transform.localScale = new Vector3 (1.1f, 1.1f, 1.1f);
-		gameObject.GetComponent<SpriteRenderer> ().sortingOrder = 1;
-	}
-	public void Drop()
-	{
-		transform.localScale = new Vector3(1, 1, 1);
-		gameObject.GetComponent<SpriteRenderer>().sortingOrder = 0;
-
-		Vector2 newPosition;
-		if(touchingTiles.Count == 0)
+	#region PRIVATE MEMBERS
+		[SerializeField]
+		private Vector2 startingPosition;
+		[SerializeField]
+		private List<Transform> touchingTiles;
+		[SerializeField]
+		private Transform myParent;		//Caching for optimisation
+		private AudioSource audSource;
+		[SerializeField]
+		private Vector3 pickUpScale = new Vector3(1.1f,1.1f,1.1f);
+		[SerializeField]
+		private Vector3 dropScale = Vector3.one;
+	#endregion
+	#region SYSTEM METHODS
+		void Start ()
 		{
-			transform.position = startingPosition;
-			transform.parent = myParent;
-			return;
+			startingPosition = transform.position;
+			touchingTiles = new List<Transform> ();
+			myParent = transform.parent;
+			audSource = gameObject.GetComponent<AudioSource> ();
 		}
-		var currentCell = touchingTiles [0];
-		if (touchingTiles.Count == 1) {
-			newPosition = currentCell.position;
-		} else 
-		{
-			float distance = Vector2.Distance(transform.position, touchingTiles [0].position);
 
-			foreach (Transform cell in touchingTiles) 
-			{
-				if (Vector2.Distance (transform.position, cell.position) < distance)
-				{
-					currentCell = cell;
-					distance = Vector2.Distance (transform.position, cell.position);
-				}	
+		void OnTriggerEnter2D (Collider2D other)
+		{
+			if (other.tag != Tags.CELL)
+				return;
+			if (!touchingTiles.Contains (other.transform)) {
+				touchingTiles.Add (other.transform);
 			}
-			newPosition = currentCell.position;
 		}
-		if (currentCell.childCount != 0) {
-			transform.position = startingPosition;
-			transform.parent = myParent;
-			return;
-		} else 
+		
+		void OnTriggerExit2D (Collider2D other)
 		{
-			transform.parent = currentCell;
-			StartCoroutine (SlotIntoPlace(transform.position,newPosition));
+			if (other.tag != Tags.CELL)
+				return;
+			if (touchingTiles.Contains (other.transform)) {
+				touchingTiles.Remove (other.transform);
+			}
 		}
-	}
+	#endregion
+	#region PUBLIC METHODS
+		public void PickUp ()
+		{
+			transform.localScale = pickUpScale;
+			gameObject.GetComponent<SpriteRenderer> ().sortingOrder = 4;
+		}
 
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		if (other.tag != "Cell") return;
-		if (!touchingTiles.Contains(other.transform))
+		public void Drop ()
 		{
-			touchingTiles.Add(other.transform);
-		}
-	}
+			transform.localScale = dropScale;
+			gameObject.GetComponent<SpriteRenderer> ().sortingOrder = 3;
 
-	void OnTriggerExit2D(Collider2D other)
-	{
-		if (other.tag != "Cell") return;
-		if (touchingTiles.Contains(other.transform))
-		{
-			touchingTiles.Remove(other.transform);
-		}
-	}
+			Vector2 newPosition;
+			if (touchingTiles.Count == 0) {
+				transform.position = startingPosition;
+				transform.SetParent(myParent);
+				return;
+			}
+			var currentCell = touchingTiles [0];
+			if (touchingTiles.Count == 1) {
+				newPosition = currentCell.position;
+			} else {
+				float distance = Vector2.Distance (transform.position, touchingTiles [0].position);
 
-	IEnumerator SlotIntoPlace(Vector2 startingPos, Vector2 endingPos)
-	{
-		float duration = 0.1f;
-		float elapsedTime = 0;
-		audSource.Play();
-		while (elapsedTime < duration)
-		{
-			transform.position = Vector2.Lerp(startingPos, endingPos, elapsedTime / duration);
-			elapsedTime += Time.deltaTime;
-			yield return new WaitForEndOfFrame();
+				foreach (Transform cell in touchingTiles) {
+					if (Vector2.Distance (transform.position, cell.position) < distance) {
+						currentCell = cell;
+						distance = Vector2.Distance (transform.position, cell.position);
+					}	
+				}
+				newPosition = currentCell.position;
+			}
+			if (currentCell.childCount != 0) {
+				transform.position = startingPosition;
+				transform.SetParent(myParent);
+				return;
+			} else {
+				transform.SetParent(currentCell);
+				StartCoroutine (SlotIntoPlace (transform.position, newPosition));
+			}
 		}
-		transform.position = endingPos;
+	#endregion
+	#region PRIVATE METHODS
+		private IEnumerator SlotIntoPlace (Vector2 startingPos, Vector2 endingPos)
+		{
+			float duration = 0.1f;
+			float elapsedTime = 0;
+			audSource.Play ();
+			while (elapsedTime < duration) {
+				transform.position = Vector2.Lerp (startingPos, endingPos, elapsedTime / duration);
+				elapsedTime += Time.deltaTime;
+				yield return new WaitForEndOfFrame ();
+			}
+			transform.position = endingPos;
+		}
+	#endregion
 	}
-}
 }
