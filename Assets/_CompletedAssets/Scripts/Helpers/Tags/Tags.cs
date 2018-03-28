e2bb6a12d173a7335bc1bbca2134166eddf9c3fa
Ulan Assanov <Ulan@users-iMac-2.local>
2018-03-28 10:29:10 +0600

    updates Tags.cs for new tags

--- Assets/_CompletedAssets/Scripts/Helpers/Tags/Tags.cs
+++ Assets/_CompletedAssets/Scripts/Helpers/Tags/Tags.cs
@@ -6,17 +6,28 @@ namespace MeezumGame
 {
 	public class Tags
 	{
-		public const string STAR_MANAGER = "StarManager";
+		public const string MAIN_CAMERA = "MainCamera";
+		public const string PLAYER = "Player";
+
 		public const string TILE = "Tile";
 		public const string CELL = "Cell";
 		public const string LETTERS = "Letters";
-		public const string CELLS = "Cells";
-		public const string LETTER_PREFAB = "LetterPrefab";
-		public const string PLAYER = "Player";
 		public const string QUESTION = "Question";
+		public const string LETTER_PREFAB = "LetterPrefab";
+		public const string CELLS = "Cells";
+		public const string STAR_MANAGER = "StarManager";
 		public const string QUIZ_UI_MANAGER = "QuizUIManager";
 		public const string SOUND_MANAGER = "SoundManager";
 		public const string INSTRUCTIONS_SOUND_MANAGER = "InstructionsSoundManager";
 		public const string NOTIFICATION_MANAGER = "NotificationManager";
+		public const string GAME_WORDS_UI_MANAGER = "GameWordsUIManager";
+		public const string CHARACTER = "Character";
+		public const string BACKGROUND_MUSIC = "BackgroundMusic";
+		public const string GAME_WORDS_MANAGER = "GameWordsManager";
+		public const string QUIZ_GAME_MANAGER = "QuizGameManager";
+		public const string GAME_PANEL = "GamePanel";
+
+
+
 	}
 }
