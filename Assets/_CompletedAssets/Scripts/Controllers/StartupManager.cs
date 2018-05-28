74bc6d8f315b77109819bab108668f6bc7ac450c
Ulan Assanov <Ulan@users-iMac-2.local>
2018-05-28 16:35:53 +0600

    sets localization functionality

---
+++ Assets/_CompletedAssets/Scripts/Controllers/StartupManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using MeezumGame;
+namespace Localization{
+public class StartupManager : MonoBehaviour {
+	
+	// Use this for initialization
+	private IEnumerator Start () 
+	{
+		while (!LocalizationManager.instance.GetIsReady ()) 
+		{
+			yield return null;
+		}
+		
+		SceneManager.LoadScene (Scenes.COMICS_SCENE);
+	}
+	
+}
+}
---
+++ Assets/_CompletedAssets/Scripts/Helpers/Constants/Scenes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MeezumGame
+{
+	public class Scenes
+	{
+		public const string COMICS_SCENE = "Comics";
+
+
+	}
+}
